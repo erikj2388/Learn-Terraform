29ba48e3e999e3ee83156bee4a403689947d7134
erikj2388 <erikj2388@gmail.com>
2021-09-24 11:30:03 -0400

    First commit

---
+++ storage-account/variables.tf
@@ -0,0 +1,15 @@
+
+variable "storageAccountName" {
+  description = "Storage account name"
+  type        = string
+}
+
+variable "resourceGroupName" {
+  description = "resource group name"
+  type        = string
+}
+
+variable "location" {
+  description = "Region"
+  type        = string
+}
