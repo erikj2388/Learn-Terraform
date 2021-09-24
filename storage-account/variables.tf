2f2847399ee55714a63dc17341610a9281f74d9f
erikj2388 <erikj2388@gmail.com>
2021-09-24 12:27:08 -0400

    Updated variables

--- storage-account/variables.tf
+++ storage-account/variables.tf
@@ -2,14 +2,17 @@
 variable "storageAccountName" {
   description = "Storage account name"
   type        = string
+  default = "skylinesstrg01"
 }
 
 variable "resourceGroupName" {
   description = "resource group name"
   type        = string
+  default = "Dev01"
 }
 
 variable "location" {
   description = "Region"
   type        = string
+  default = "eastus2"
 }
